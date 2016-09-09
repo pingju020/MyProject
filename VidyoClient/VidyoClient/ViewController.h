16c08f1db503308bc620426c37975bc7c781cfdd
pingju020 <441914317@qq.com>
2016-09-09 09:25:29 +0800

    添加VidyoClient客户端
    添加VidyoClient客户端

---
+++ VidyoClient/VidyoClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VidyoClient
+//
+//  Created by yangjuanping on 16/9/9.
+//  Copyright © 2016年 yangjuanping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
